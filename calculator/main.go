bd2b092b5a08d99ca63dfc288299aa520ec086da
Tony Lawrence <rigatoniman@hotmail.com>
2022-05-30 12:27:30 -0700

    edit: add calutor with simple choice operator

--- calculator/main.go
+++ calculator/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func calculateEquation(v string, val1 float64, val2 float64) (sum float64) {
+
+	switch strings.TrimSpace(v) {
+	case "+":
+		sum = val1 + val2
+		break
+	case "-":
+		sum = val1 - val2
+		break
+	case "*", "x":
+		sum = val1 * val2
+		break
+	case "/":
+		sum = val1 / val2
+		break
+	default:
+		panic("You passed in an unsupported operator please in operators +, -, *, or /")
+	}
+	return sum
+}
 
 func main() {
-	fmt.Println("A simple calculator")
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Value 1: ")
+	input1, _ := reader.ReadString('\n')
+	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
+
+	if err != nil {
+		fmt.Println(err)
+		panic("Value 1 must be a number")
+	}
+
+	fmt.Print("Operator: ")
+	inputOp, _ := reader.ReadString('\n')
+
+	fmt.Print("Value 2: ")
+	input2, _ := reader.ReadString('\n')
+	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+	if err != nil {
+		fmt.Println(err)
+		panic("Value 2 must be a number")
+	}
+
+	sum := calculateEquation(inputOp, float1, float2)
+	sum = math.Round(sum*100) / 100
+	fmt.Printf("The sum of %v %v %v is %v\n\n", float1, inputOp, float2, sum)
 }
--- practice/main.go
+++ practice/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"time"
 )
 
@@ -10,11 +11,26 @@ func main() {
 	n := time.Now()
 	fmt.Println("I recorded this video at ", n)
 
-	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.UTC)
+	t := time.Date(time.Now().Year(), time.Now().Month(), time.Now().Day(), time.Now().Hour(), time.Now().Minute(), time.Now().Second(), time.Now().Nanosecond(), time.UTC)
 	fmt.Println("Go launched at ", t)
 	fmt.Println(t.Format(time.ANSIC))
 
-	parsedTime, _ := time.Parse(time.ANSIC, "Tue Nov 10 23:00:00 2009")
+	parsedTime, _ := time.Parse(time.ANSIC, t.String())
 	fmt.Printf("The type of parsedTime is %T\n", parsedTime)
 
+	i1, i2, i3 := 12, 45, 68
+	intSum := i1 + i2 + i3
+
+	fmt.Println("Integer Sum:", intSum)
+
+	f1, f2, f3 := 23.5, 65.1, 76.3
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float sum:", floatSum)
+
+	floatSum = math.Round(floatSum*100) / 100
+	fmt.Println("The sum is now, floatSum")
+
+	circleRadius := 15.5
+	circumference := circleRadius * 2 * circleRadius
+	fmt.Printf("Circumference: %.2f\n", circumference)
 }
