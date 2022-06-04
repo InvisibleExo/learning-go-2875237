b951216a70b479c62d803d5ad24c69ad8c716e9c
Tony Lawrence <rigatoniman@hotmail.com>
2022-06-04 08:49:27 -0700

    edit: explore struct type

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Structs")
+	poodle := Dog{"Rat Terrier", 20}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	poodle.Weight = 25
+	fmt.Printf("Breed: %+v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+}
+
+// dog is a struct
+type Dog struct {
+	Breed  string
+	Weight int
 }
