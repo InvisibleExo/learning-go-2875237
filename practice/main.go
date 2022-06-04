4f52c9791b57abe6af8bc5afe3f265f94c42da99
Tony Lawrence <rigatoniman@hotmail.com>
2022-06-04 07:48:13 -0700

    edit: explore pointer and memory logic

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	var p = &anInt
+	fmt.Println("Value of p:", *p)
+
+	value1 := 42.13
+	pointer1 := &value1
+	fmt.Println("Value fo pointer1:", *pointer1, *p)
+
+	*pointer1 = *pointer1 / 31
+	fmt.Println("Value fo pointer1:", *pointer1)
+	fmt.Println("Value fo value1:", value1)
 }
