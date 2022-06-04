0c13972681cec2359e5f4a40e530c3c8563d0811
Tony Lawrence <rigatoniman@hotmail.com>
2022-06-04 08:12:14 -0700

    edit: explore with arrays

--- practice/main.go
+++ practice/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors = []string{"Red", "Green", "Blue"}
+	fmt.Println(colors)
+	fmt.Println(colors[0])
+	colors = append(colors, "Purple")
+	fmt.Println(colors)
+
+	colors = append(colors[1:])
+	fmt.Println(colors)
+
+	colors = append(colors[:len(colors)-1])
+	fmt.Println(colors)
+
+	numbers := make([]int, 5)
+	numbers[0] = 134
+	numbers[1] = 78
+	numbers[3] = 98
+	fmt.Println(numbers)
+
+	sort.Ints(numbers)
+	fmt.Println(numbers)
+
 }
