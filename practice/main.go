a191839579a1ccc78beffda7426a9ac5c0ee70ed
Tony Lawrence <rigatoniman@hotmail.com>
2022-06-11 13:20:19 -0700

    edit: make methods from struct types

--- practice/main.go
+++ practice/main.go
@@ -5,14 +5,38 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{Breed: "Terrier", Weight: 10, Sound: "Oink Oink Woof!"}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	poodle.Speak()
+	poodle.SpeakThreeTimes()
+	poodle.SpeakThreeTimesFromMemory()
+	poodle.SpeakThreeTimesFromMemory()
+	poodle.SpeakThreeTimesFromMemory()
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
+}
+
+// Speak is how the dog speaks
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
+}
+
+// Lets the dog speak 3 times
+func (d Dog) SpeakThreeTimes() {
+	d.Sound = fmt.Sprintf("1: %v :: 2: %v :: 3: %v ::", d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
+}
+
+// Lets the dog speak 3 times and what it remembers from before
+func (d *Dog) SpeakThreeTimesFromMemory() {
+	d.Sound = fmt.Sprintf("1: %v :: 2:  %v :: 3: %v ::", d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
 }
