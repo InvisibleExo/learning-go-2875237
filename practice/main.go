db596f5bf8e88f79c327a335a88ce84c210d75d5
Tony Lawrence <rigatoniman@hotmail.com>
2022-06-11 12:53:18 -0700

    edit: define syntax of functions and invoke said function

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,25 @@ import (
 )
 
 func main() {
-	fmt.Println("Functions")
+	doSomthing()
+
+}
+
+func doSomthing() {
+	fmt.Println("Doing something from another function.")
+	fmt.Println(addValues(4, 5))
+	fmt.Println(addAllValues(5, 6, 6, 9, 10, 20))
+}
+
+func addValues(val1, val2 int) (sum int) {
+	sum = val1 + val2
+	return sum
+}
+
+func addAllValues(values ...int) (int, int) {
+	total := 0
+	for _, v := range values {
+		total += v
+	}
+	return total, len(values)
 }
